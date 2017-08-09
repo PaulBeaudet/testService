7a24a404ec533f45b7f6eaf6777a1bc30890125d
Paul Beaudet <inof8or@gmail.com>
2017-08-09 16:19:15 -0400

    initial commit

---
+++ testService.js
@@ -0,0 +1,26 @@
+// testService.js service for testing the services
+
+var test = {
+    response: function(){
+        return function(req, res){
+            res.send('hello world');
+        }
+    }
+}
+
+var serve = {                                                // handles express server setup
+    express: require('express'),                             // server framework library
+    parse: require('body-parser'),                           // middleware to parse JSON bodies
+    theSite: function (){                                    // methode call to serve site
+        var app = serve.express();                           // create famework object
+        var http = require('http').Server(app);              // http server for express framework
+        app.use(serve.parse.json());                         // support JSON bodies
+        var router = serve.express.Router();                 // create express router object to add routing events to
+        router.get('/', test.response());                   // real listener post route
+        app.use(router);                                     // get express to user the routes we set
+        return http;
+    }
+};
+
+var http = serve.theSite();                                  // set express middleware and routes up
+http.listen(process.env.PORT);                               // listen on specified PORT enviornment variable
