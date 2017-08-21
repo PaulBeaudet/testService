dedadd9f1b0036986389c28851a9ddb1d1128efb
Paul Beaudet <inof8or@gmail.com>
2017-08-21 16:16:35 -0400

    I hope this works

--- testService.js
+++ testService.js
@@ -4,7 +4,7 @@ var test = {
     response: function(){
         return function(req, res){
             // res.send(process.env.MESSAGE);
-            res.send('ur a towel');
+            res.send('omg just work');
         }
     },
     otherRes: function(){
