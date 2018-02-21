45c5ab4ec813482ce6888f4f6dbc20f899e1dabf
PaulBeaudet <inof8or@gmail.com>
2018-02-20 21:17:24 -0500

    formating

--- testService.js
+++ testService.js
@@ -13,23 +13,23 @@ var test = {
     }
 };
 
-var serve = {                                                // handles express server setup
-    express: require('express'),                             // server framework library
-    parse: require('body-parser'),                           // middleware to parse JSON bodies
-    theSite: function (){                                    // methode call to serve site
-        var app = serve.express();                           // create famework object
-        var http = require('http').Server(app);              // http server for express framework
-        app.use(serve.parse.json());                         // support JSON bodies
-        var router = serve.express.Router();                 // create express router object to add routing events to
-        router.get('/', test.response());                    // real listener post route
+var serve = {                                   // handles express server setup
+    express: require('express'),                // server framework library
+    parse: require('body-parser'),              // middleware to parse JSON bodies
+    theSite: function (){                       // methode call to serve site
+        var app = serve.express();              // create famework object
+        var http = require('http').Server(app); // http server for express framework
+        app.use(serve.parse.json());            // support JSON bodies
+        var router = serve.express.Router();    // create express router object to add routing events to
+        router.get('/', test.response());       // real listener post route
         router.get('/test', test.otherRes());
-        app.use(router);                                     // get express to user the routes we set
+        app.use(router);                        // get express to user the routes we set
         return http;
     }
 };
 
-var http = serve.theSite();                                  // set express middleware and routes up
-http.listen(process.env.PORT);                               // listen on specified PORT enviornment variable
+var http = serve.theSite();                     // set express middleware and routes up
+http.listen(process.env.PORT);                  // listen on specified PORT enviornment variable
 
 var pkgjson = require('./package.json');
 console.log('Starting ' + pkgjson.name + ' version ' + pkgjson.version); // show version of package when restarted
